e727104dae1a33c26b226c444a552999f918cfb1
Isa-Gomes-Ferreira <102129758+Isa-Gomes-Ferreira@users.noreply.github.com>
2022-04-25 10:29:24 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-﻿Conta c1 = new Conta(1);
+Conta c1 = new Conta(1);
 Conta c2 = new Conta(2);
 
 c1.Depositar(1000.00);
 c1.Sacar(200.00);
+c1.Transferir(100, c2);
 Console.WriteLine(c1.Saldo);
 
 c2.Depositar(200.00);
